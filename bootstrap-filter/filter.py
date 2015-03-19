09d2a58eb5601b92d6317870788b0ce7cffd46b5
Raphael Lopez Kaufman <raphael.lopezkaufman@gmail.com>
2015-03-19 12:57:48 +0000

    add docstrings

--- bootstrap-filter/filter.py
+++ bootstrap-filter/filter.py
@@ -3,11 +3,6 @@ import numpy
 
 numpy.seterr(over="raise", under="raise", invalid="raise")
 
-
-def _normalize(l, w):
-    total = sum(l)
-    return [item/total for item in l]
-
 def bootstrap_filter(param, start, end, N, kernel_density, conditional_density, proposal, proposal_density, initial, observations):
     particles_all = numpy.empty(shape=(end-start, N))
     particles = []
@@ -17,7 +12,7 @@ def bootstrap_filter(param, start, end, N, kernel_density, conditional_density, 
     ESS = []
 
     for i in range(N):
-        particles.append(initial(param['initial']))
+        particles.append(initial(param['initial'])) #draw from initial distribution
         weights[i] = 1.0/N
     likelihood = -numpy.log(N) + numpy.log(sum(weights))
     likelihoods.append(likelihood)
@@ -25,7 +20,7 @@ def bootstrap_filter(param, start, end, N, kernel_density, conditional_density, 
     for i in range(start+1, end+1):
         numpy.divide(weights, sum(weights), out=weights)
 
-        try:
+        try: #catch underflow error
             ESS.append(1/sum([weight**2 for weight in weights]))
         except FloatingPointError:
             ESS.append(0)
@@ -33,9 +28,14 @@ def bootstrap_filter(param, start, end, N, kernel_density, conditional_density, 
         indices = numpy.random.multinomial(N, weights, size=1)[0]
         ancestors = [particles[j] for j in range(len(indices)) for _ in range(indices[j])]
         particles = [proposal(ancestor, observations[i], param['proposal']) for ancestor in ancestors]
-        denom = [proposal_density(particles[ancestors.index(ancestor)], ancestor, observations[i], param['proposal']) for ancestor in ancestors]
-        obs = [conditional_density(particle, observations[i], param['conditional']) for particle in particles]
-        transi = [kernel_density(particles[ancestors.index(ancestor)], ancestor, param['kernel']) for ancestor in ancestors]
+
+        denom = [proposal_density(particles[ancestors.index(ancestor)],
+                                  ancestor, observations[i], param['proposal']) for ancestor in ancestors] #q(x_t|x_t-1, y_t)
+        obs = [conditional_density(particle, observations[i],
+                                   param['conditional']) for particle in particles] #p(y_t[x_t)
+        transi = [kernel_density(particles[ancestors.index(ancestor)],
+                                 ancestor, param['kernel']) for ancestor in ancestors] #p(x_t|x_t-1)
+
         weights = numpy.zeros(N)
         numpy.multiply(obs, transi, out=weights)
         numpy.divide(weights, denom, out=weights)
