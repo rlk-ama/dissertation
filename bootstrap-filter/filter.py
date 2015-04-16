6d0ad15ef6f04a53b5cd4103d4588db64eb12e67
Raphael Lopez Kaufman <raphael.lopezkaufman@gmail.com>
2015-04-16 12:14:07 +0100

    refactoring

--- bootstrap-filter/filter.py
+++ bootstrap-filter/filter.py
@@ -3,53 +3,59 @@ import numpy
 
 numpy.seterr(over="raise", under="raise", invalid="raise")
 
-def bootstrap_filter(param, start, end, N, kernel_density, conditional_density, proposal, proposal_density, initial, observations):
-    particles_all = numpy.empty(shape=(end-start, N))
-    particles = []
+class BootstrapFilter(object):
 
-    weights = numpy.empty(shape=N, dtype='float64')
-    likelihoods = []
-    ESS = []
-    likelihood = 0
+    def __init__(self, start, end, N, Map):
+        self.start = start
+        self.end = end
+        self.N = N
+        self.Map = Map
+        self.observations, self.state = self.observ_gen()
 
-    for i in range(N):
-        particles.append(initial(param['initial'])) #draw from initial distribution
-        weights[i] = 1.0/N
+    def observ_gen(self):
+        observ = []
+        state = []
+        x = self.Map.initial()
+        for i in range(self.end+1):
+            observ.append(self.Map.conditional(x))
+            state.append(x)
+            x = self.Map.kernel(x)
+        return observ, state
 
-    for i in range(start+1, end+1):
-        numpy.divide(weights, sum(weights), out=weights)
+    def filter(self):
+        particles_all = numpy.empty(shape=(self.end-self.start, self.N))
+        particles = []
 
-        try: #catch underflow error
-            ESS.append(1/sum([weight**2 for weight in weights]))
-        except FloatingPointError:
-            ESS.append(0)
+        weights = numpy.empty(shape=self.N, dtype='float64')
+        likelihoods = []
+        ESS = []
+        likelihood = 0
 
-        indices = numpy.random.multinomial(N, weights, size=1)[0]
-        ancestors = [particles[j] for j in range(len(indices)) for _ in range(indices[j])]
-        particles = [proposal(ancestor, observations[i], param['proposal']) for ancestor in ancestors]
+        for i in range(self.N):
+            particles.append(self.Map.initial()) #draw from initial distribution
+            weights[i] = 1.0/self.N
 
-        denom = [proposal_density(particles[ancestors.index(ancestor)],
-                                  ancestor, observations[i], param['proposal']) for ancestor in ancestors] #q(x_t|x_t-1, y_t)
-        obs = [conditional_density(particle, observations[i],
-                                   param['conditional']) for particle in particles] #p(y_t[x_t)
-        transi = [kernel_density(particles[ancestors.index(ancestor)],
-                                 ancestor, param['kernel']) for ancestor in ancestors] #p(x_t|x_t-1)
+        for i in range(self.start+1, self.end+1):
+            numpy.divide(weights, sum(weights), out=weights)
 
-        weights = numpy.zeros(N)
-        numpy.multiply(obs, transi, out=weights)
-        numpy.divide(weights, denom, out=weights)
-        likelihood += -numpy.log(N) + numpy.log(sum(weights))
-        likelihoods.append(likelihood)
-        particles_all[i-1] = ancestors
+            try: #catch underflow error
+                ESS.append(1/sum([weight**2 for weight in weights]))
+            except FloatingPointError:
+                ESS.append(0)
 
-    return particles_all, likelihoods, ESS
+            indices = numpy.random.multinomial(self.N, weights, size=1)[0]
+            ancestors = [particles[j] for j in range(len(indices)) for _ in range(indices[j])]
+            particles = [self.Map.proposal(ancestor, self.observations[i]) for ancestor in ancestors]
 
-def observ_gen(N, params, conditional, initial, kernel):
-    observ = []
-    state = []
-    x = initial(params['initial'])
-    for i in range(N+1):
-        observ.append(conditional(x, params['conditional']))
-        state.append(x)
-        x = kernel(x, params['kernel'])
-    return observ, state
+            denom = [self.Map.proposal_density(particles[ancestors.index(ancestor)], ancestor, self.observations[i]) for ancestor in ancestors] #q(x_t|x_t-1, y_t)
+            obs = [self.Map.conditional_density(particle, self.observations[i]) for particle in particles] #p(y_t[x_t)
+            transi = [self.Map.kernel_density(particles[ancestors.index(ancestor)], ancestor) for ancestor in ancestors] #p(x_t|x_t-1)
+
+            weights = numpy.zeros(self.N)
+            numpy.multiply(obs, transi, out=weights)
+            numpy.divide(weights, denom, out=weights)
+            likelihood += -numpy.log(self.N) + numpy.log(sum(weights))
+            likelihoods.append(likelihood)
+            particles_all[i-1] = ancestors
+
+        return particles_all, likelihoods, ESS
--- bootstrap-filter/ricker_gamma.py
+++ bootstrap-filter/ricker_gamma.py
@@ -1,153 +1,77 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.optimize import broyden1, fsolve
 from scipy.special import psi, gamma, polygamma, gammaln
-from filter import bootstrap_filter, observ_gen
 
-r = np.exp(3.5)
-phi = 10
-sigma = np.sqrt(0.3)
+class RickerMap(object):
 
-def kernel(n, param):
-    r = param['r']
-    sigma = param['sigma']
-    out = np.random.lognormal(mean=param['mean'], sigma=sigma)
-    return r*n*np.exp(-n)*out
+    def __init__(self, r, phi, mu, sigma, initial, approx='simple'):
+        self.r = r
+        self.phi = phi
+        self.mu = mu
+        self.sigma = sigma
+        self.initial = initial
+        self.approx = approx
 
-def kernel_density(n, n_prev, param):
-    r = param['r']
-    sigma = param['sigma']
-    mu = param['mean']
-    coeff = r*n_prev*np.exp(-n_prev)
-    mean = mu + np.log(coeff)
-    out = 1/(n*sigma*np.sqrt(2*np.pi))*np.exp(1/(2*sigma**2)*(np.log(n)-mean)**2)
-    return out
+    def kernel(self, n):
+        out = np.random.lognormal(mean=self.mu, sigma=self.sigma)
+        return self.r*n*np.exp(-n)*out
 
-def kernel_prior(n, obs, param):
-    r = param['r']
-    sigma = param['sigma']
-    out = np.random.lognormal(mean=param['mean'], sigma=sigma)
-    return r*n*np.exp(-n)*out
+    def kernel_density(self, n, n_prev):
+        coeff = self.r*n_prev*np.exp(-n_prev)
+        mean = self.mu + np.log(coeff)
+        out = 1/(n*self.sigma*np.sqrt(2*np.pi))*np.exp(1/(2*self.sigma**2)*(np.log(n)-mean)**2)
+        return out
 
-def kernel_density_prior(n, n_prev, obs, param):
-    r = param['r']
-    sigma = param['sigma']
-    mu = param['mean']
-    coeff = r*n_prev*np.exp(-n_prev)
-    mean = mu + np.log(coeff)
-    out = 1/(n*sigma*np.sqrt(2*np.pi))*np.exp(1/(2*sigma**2)*(np.log(n)-mean)**2)
-    return out
+    def kernel_prior(self, n, obs):
+        out = np.random.lognormal(mean=self.mu, sigma=self.sigma)
+        return self.r*n*np.exp(-n)*out
 
-def conditional(n, param):
-    phi = param['phi']
-    out = np.random.poisson(lam=phi*n)
-    return out
+    def kernel_density_prior(self, n, n_prev, obs):
+        coeff = self.r*n_prev*np.exp(-n_prev)
+        mean = self.mu + np.log(coeff)
+        out = 1/(n*self.sigma*np.sqrt(2*np.pi))*np.exp(1/(2*self.sigma**2)*(np.log(n)-mean)**2)
+        return out
 
-def proposal(n, y, param):
-    r = param['r']
-    sigma = param['sigma']
-    mu = param['mean']
-    alpha, beta = param_gamma(n, r, mu, sigma)
-    out = np.random.gamma(shape=y+alpha, scale=beta/(phi*beta+1))
-    return out
+    def conditional(self, n):
+        out = np.random.poisson(lam=self.phi*n)
+        return out
 
-def initial(param):
-    out = np.random.gamma(shape=param['shape'], scale=param['scale'])
-    return out
+    def proposal(self, n, y):
+        if self.approx == 'simple':
+            alpha, beta = self.param_gamma(n)
+        else:
+            alpha, beta = self.param_gamma2(n)
+        out = np.random.gamma(shape=y+alpha, scale=beta/(self.phi*beta+1))
+        return out
 
-def proposal_density(n, n_prev, y, param):
-    sigma = param['sigma']
-    mu = param['mean']
-    r = param['r']
-    alpha, beta = param_gamma(n_prev, r, mu, sigma)
-    outlog= (alpha+y)*np.log((phi*beta+1)/beta) - gammaln(alpha+y) + (alpha+y-1)*np.log(n) - ((phi*beta+1)/beta)*n
-    #out = ((phi*beta+1)/beta)**(alpha+y)/gamma(alpha+y)*n**(alpha+y-1)*np.exp(-((phi*beta+1)/beta)*n)
-    out = np.exp(outlog)
-    return  out
+    def proposal_density(self, n, n_prev, y):
+        if self.approx == 'simple':
+            alpha, beta = self.param_gamma(n_prev)
+        else:
+            alpha, beta = self.param_gamma2(n_prev)
+        outlog= (alpha+y)*np.log((self.phi*beta+1)/beta) - gammaln(alpha+y) + (alpha+y-1)*np.log(n) - ((self.phi*beta+1)/beta)*n
+        #out = ((phi*beta+1)/beta)**(alpha+y)/gamma(alpha+y)*n**(alpha+y-1)*np.exp(-((phi*beta+1)/beta)*n)
+        out = np.exp(outlog)
+        return  out
 
-def conditional_density(n, y, param):
-    phi = param['phi']
-    outlog = -phi*n + y*np.log(phi*n) - gammaln(y+1)
-    #out = np.exp(-phi*n)*(phi*n)**y/np.math.factorial(y)
-    out = np.exp(outlog)
-    return out
+    def conditional_density(self, n, y):
+        outlog = -self.phi*n + y*np.log(self.phi*n) - gammaln(y+1)
+        #out = np.exp(-phi*n)*(phi*n)**y/np.math.factorial(y)
+        out = np.exp(outlog)
+        return out
 
-def param_gamma(n_prev, r, mu, sigma):
-    coeff = r*n_prev*np.exp(-n_prev)
-    alpha = 1/sigma**2
-    beta = 1/alpha*np.exp(mu+np.log(coeff)+sigma**2/2)
-    return alpha, beta
+    def param_gamma(self, n_prev):
+        coeff = self.r*n_prev*np.exp(-n_prev)
+        alpha = 1/self.sigma**2
+        beta = 1/alpha*np.exp(self.mu+np.log(coeff)+self.sigma**2/2)
+        return alpha, beta
 
-def param_gamma2(n_prev, r, mu, sigma):
+    def param_gamma2(self, n_prev):
 
-    def p(k):
-        return -k**3 + ((sigma**2-1)/(3*sigma**2))*k**2 + k/sigma**2 - ((15+2*sigma**2)/(30*sigma**2))
+        def p(k):
+            return -k**3 + ((self.sigma**2-1)/(3*self.sigma**2))*k**2 + k/self.sigma**2 - ((15+2*self.sigma**2)/(30*self.sigma**2))
 
-    coeff = r*n_prev*np.exp(-n_prev)
-    alpha = fsolve(p, 0.01)[0]
-    beta = 1/alpha*np.exp(mu+np.log(coeff)+ 1/(2*alpha))
-    return alpha, beta
-
-if __name__ == '__main__':
-     #liks = []
-     # liks2 = []
-     # rs = map(np.exp, np.linspace(2.5,4.5, 190))
-     # for r in rs:
-
-    params = {
-    'initial': {'shape': 3, 'scale': 1},
-    'kernel': {'r': r, 'sigma': sigma, 'mean': 0.0},
-    'proposal': {'r': r, 'sigma': sigma, 'mean': 0.0},
-    'conditional': {'phi': phi}
-    }
-
-    observ, state = observ_gen(30, params, conditional=conditional, initial=initial, kernel=kernel)
-
-    estim, likeli, ESS = bootstrap_filter(param=params, start=0, end=30, N=1000, kernel_density=kernel_density, conditional_density=conditional_density,
-                            proposal=proposal, proposal_density=proposal_density, initial=initial,
-                            observations=observ)
-
-        # estim2, likeli2, ESS2 = bootstrap_filter(param=params, start=0, end=30, N=100, kernel_density=kernel_density, conditional_density=conditional_density,
-        #                        proposal=kernel_prior, proposal_density=kernel_density_prior, initial=initial,
-        #                        observations=observ)
-        # liks.append(likeli[-1])
-        #liks2.append(likeli2[-1])
-
-    fig, ax1 = plt.subplots()
-    #fig1 = plt.figure()
-    #mean_esti = [np.mean(est) for est in estim]
-    # ax1.plot([i for i in range(30)], mean_esti)
-    #plt.plot([i for i in range(30)], mean_esti)
-    ax1.plot([i for i in range(31)], state)
-    # plt.savefig('diagno_gamma.pdf')
-    # plt.show()
-    # plt.close()
-    ax2 = ax1.twinx()
-    ax2.plot([i for i in range(30)], ESS, color="red")
-    # plt.savefig('ESS_gamma.pdf')
-    # plt.show()
-    # plt.close()
-    #fig3 = plt.figure()
-    #plt.plot(rs, liks)
-    #plt.plot(rs, liks2)
-    #plt.show()
-    #plt.plot([i for i in range(30)], likeli)
-    # plt.savefig('loglik_gamma.pdf')
-    # plt.close()
-    #
-    #mean_esti = [np.mean(est) for est in estim2]
-    # fig1 = plt.figure()
-    #plt.plot([i for i in range(30)], mean_esti)
-    #plt.show()
-    #plt.plot([i for i in range(31)], state)
-    # plt.savefig('diagno_prior.pdf')
-    # plt.close()
-    #fig2 = plt.figure()
-    #plot([i for i in range(30)], ESS)
-    # plt.savefig('ESS_prior.pdf')
-    # plt.close()
-    # fig3 = plt.figure()
-    #plt.plot([i for i in range(30)], likeli2)
-    # plt.savefig('loglik_prior.pdf')
-    # plt.close()
-    plt.show()
+        coeff = self.r*n_prev*np.exp(-n_prev)
+        alpha = fsolve(p, 0.01)[0]
+        beta = 1/alpha*np.exp(self.mu+np.log(coeff)+ 1/(2*alpha))
+        return alpha, beta
