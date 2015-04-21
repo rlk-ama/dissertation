c3fcb826afb016cc544ad42c15a129dd75be609f
Raphael Lopez Kaufman <raphael.lopezkaufman@gmail.com>
2015-04-21 19:37:38 +0100

    add option to use prior proposal

--- bootstrap-filter/filter.py
+++ bootstrap-filter/filter.py
@@ -5,12 +5,13 @@ numpy.seterr(over="raise", under="raise", invalid="raise")
 
 class BootstrapFilter(object):
 
-    def __init__(self, start, end, N, Map):
+    def __init__(self, start, end, N, Map, prior=False):
         self.start = start
         self.end = end
         self.N = N
         self.Map = Map
         self.observations, self.state = self.observ_gen()
+        self.prior = prior
 
     def observ_gen(self):
         observ = []
@@ -45,9 +46,15 @@ class BootstrapFilter(object):
 
             indices = numpy.random.multinomial(self.N, weights, size=1)[0]
             ancestors = [particles[j] for j in range(len(indices)) for _ in range(indices[j])]
-            particles = [self.Map.proposal(ancestor, self.observations[i]) for ancestor in ancestors]
+            if self.prior:
+                particles = [self.Map.prior_proposal(ancestor, self.observations[i]) for ancestor in ancestors]
+            else:
+                particles = [self.Map.proposal(ancestor, self.observations[i]) for ancestor in ancestors]
 
-            denom = [self.Map.proposal_density(particles[ancestors.index(ancestor)], ancestor, self.observations[i]) for ancestor in ancestors] #q(x_t|x_t-1, y_t)
+            if self.prior:
+                denom = [self.Map.prior_proposal_density(particles[ancestors.index(ancestor)], ancestor, self.observations[i]) for ancestor in ancestors] #q(x_t|x_t-1, y_t)
+            else:
+                denom = [self.Map.proposal_density(particles[ancestors.index(ancestor)], ancestor, self.observations[i]) for ancestor in ancestors]
             obs = [self.Map.conditional_density(particle, self.observations[i]) for particle in particles] #p(y_t[x_t)
             transi = [self.Map.kernel_density(particles[ancestors.index(ancestor)], ancestor) for ancestor in ancestors] #p(x_t|x_t-1)
 
