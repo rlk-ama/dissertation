25f4430770c817de4cbff3531a386f164e5ec71d
Raphael Lopez Kaufman <raphael.lopezkaufman@gmail.com>
2015-06-12 22:27:47 +0100

    when all weights are equal to zero skip step

--- bootstrap/filter.py
+++ bootstrap/filter.py
@@ -3,7 +3,6 @@ import sys
 
 from math import log, exp
 
-#np.seterr(over="raise", under="raise", invalid="raise")
 DTYPE = np.float64
 
 class BootstrapFilter(object):
@@ -31,9 +30,7 @@ class BootstrapFilter(object):
             weights[i] = 1.0/N
 
         for i in range(self.start+1, self.end+1):
-            #weights = np.array([weight if weight < sys.float_info[0]/(2*len(weights)) else sys.float_info[0]/(2*len(weights)) for weight in weights])
             np.divide(weights, sum(weights), out=weights)
-            #weights = np.array([weight if not np.isnan(weight) else sys.float_info[0] for weight in weights], dtype=np.float64)
             ESS.append(1/sum([weight**2 for weight in weights]))
 
             indices = np.random.multinomial(N, weights, size=1)[0]
@@ -41,25 +38,20 @@ class BootstrapFilter(object):
             if prior:
                 particles = np.array([self.Map.prior.sample(ancestor) for ancestor in ancestors], dtype=DTYPE)
             else:
-                #particles = np.array([self.Map.proposal.sample(ancestor, self.observations[i]) for ancestor in ancestors],
-                                     #dtype=DTYPE)
                 particles = self.Map.proposal.sample(ancestors, self.observations[i])
 
             if prior:
                 denom = self.Map.prior.density(particles, ancestors) #q(x_t|x_t-1, y_t)
             else:
-                #denom = [self.Map.proposal.density(particles[j], ancestors[j], self.observations[i]) for j in range(len(ancestors))]è
                 denom = self.Map.proposal.density(particles, ancestors, self.observations[i])
-            #obs = np.array([self.Map.conditional.density(particle, self.observations[i]) for particle in particles],
-                             #dtype=DTYPE) #p(y_t[x_t)
             obs = self.Map.conditional.density(particles, self.observations[i])
-            #obs = [ob if ob != sys.float_info[3] else 0 for ob in obs ]
-            #transi = np.array([self.Map.kernel.density(particles[j], ancestors[j]) for j in range(len(ancestors))],
-              #dtype=DTYPE) #p(x_t|x_t-1)
             transi = self.Map.kernel.density(particles, ancestors)
-            weights = np.zeros(N)
-            np.multiply(obs, transi, out=weights)
-            np.divide(weights, denom, out=weights)
+            weights_n = np.zeros(N)
+            np.multiply(obs, transi, out=weights_n)
+            np.divide(weights_n, denom, out=weights_n)
+            if sum(weights_n) == 0:
+                continue
+            weights = np.copy(weights_n)
             likelihood += -np.log(N) + np.log(sum(weights))
             likelihoods.append(likelihood)
             particles_all[i-1] = ancestors
