9ecdcd7e5eac5a802a9ec12c90944032d4c0417d
Raphael Lopez Kaufman <raphael.lopezkaufman@gmail.com>
2015-06-24 19:19:27 +0100

    filter for gaussian ssm is now compliant with new distributions

--- bootstrap/kalman.py
+++ bootstrap/kalman.py
@@ -1,26 +1,29 @@
 import numpy as np
 
-from distributions.distributions import Normal
+from distributions.distributions2 import Normal
+from utils.utilsc import wrapper_arr, wrapper_arr_arr
+from bootstrap.utils_kalman import func_loc_state, func_scale_state, func_scale_obs, func_loc_proposal, func_scale_proposal
+from functools import partial
 
 class KalmanMap(object):
 
-    def __init__(self, phi, sigma_state, sigma_obs, initial=None, observations=None, length=None):
+    def __init__(self, phi, scale_state, scale_obs, initial=None, observations=None, length=None):
         self.phi = phi
-        self.sigma_state = sigma_state
-        self.sigma_obs = sigma_obs
-        self.coeff = (self.sigma_obs*self.sigma_state)**2/(self.sigma_obs**2+self.sigma_state**2)
+        self.scale_state = scale_state
+        self.scale_obs = scale_obs
+        self.scale = (self.scale_obs*self.scale_state)**2/(self.scale_obs**2+self.scale_state**2)
+
         if initial:
             self.initial = initial
         else:
-            self.initial = Normal()
+            self.initial = Normal().sample(loc=np.array([0], dtype=np.float64), scale=np.array([1], dtype=np.float64))
 
-        self.kernel = self.Kernel(Normal(func_loc=lambda args: self.phi*args,
-                                            func_scale=lambda args: self.sigma_state))
+        self.kernel = self.Kernel(Normal(), self.phi, self.scale_state)
         self.prior = self.kernel
-        self.conditional = self.Conditional(Normal(func_scale=lambda args: self.sigma_obs))
-        self.proposal = self.Proposal(Normal(func_loc=lambda x, y: self.coeff*(self.phi*x/self.sigma_state**2 + y/self.sigma_obs**2),
-                                             func_scale=lambda args: np.sqrt(self.coeff)))
-        if observations:
+        self.conditional = self.Conditional(Normal(), self.scale_obs)
+        self.proposal = self.Proposal(Normal(), self.phi, self.scale_state, self.scale_obs, self.scale)
+
+        if observations is not None:
             self.observations = observations
         else:
             self.observations, self.state = self.observ_gen(length)
@@ -28,87 +31,66 @@ class KalmanMap(object):
 
     class Kernel(object):
 
-        def __init__(self, distribution):
+        def __init__(self, distribution, phi, scale):
             self.distribution = distribution
+            self.phi = phi
+            self.scale = scale
+            self.func_loc = partial(func_loc_state, self.phi)
 
         def sample(self, ancestor):
-            return self.distribution.sample(args_loc=ancestor)
+            loc = wrapper_arr(self.func_loc, ancestor)
+            scale = wrapper_arr(func_scale_state, np.array([self.scale]*len(ancestor), dtype=np.float64))
+            return self.distribution.sample(loc=loc, scale=scale)
 
         def density(self, particle, ancestor):
-            return self.distribution.density(particle, args_loc=ancestor)
+            loc = wrapper_arr(self.func_loc, ancestor)
+            scale = wrapper_arr(func_scale_state, np.array([self.scale]*len(ancestor), dtype=np.float64))
+            return self.distribution.density(particle, loc=loc, scale=scale)
 
     class Conditional(object):
 
-        def __init__(self, distribution):
+        def __init__(self, distribution, scale):
             self.distribution = distribution
+            self.scale = scale
 
         def sample(self, ancestor):
-            return self.distribution.sample(args_loc=ancestor)
+            scale = wrapper_arr(func_scale_obs, np.array([self.scale]*len(ancestor), dtype=np.float64))
+            return self.distribution.sample(loc=ancestor, scale=scale)
 
         def density(self, particle, observation):
-            return self.distribution.density(observation, args_loc=particle)
+            observations = np.array([observation]*len(particle), dtype=np.float64)
+            scale = wrapper_arr(func_scale_obs, np.array([self.scale]*len(particle), dtype=np.float64))
+            return self.distribution.density(observations, loc=particle, scale=scale)
 
     class Proposal(object):
 
-        def __init__(self, distribution):
+        def __init__(self, distribution, phi, scale_state, scale_obs, scale):
             self.distribution = distribution
+            self.phi = phi
+            self.scale_state= scale_state
+            self.scale_obs = scale_obs
+            self.scale = scale
+            self.func_loc = partial(func_loc_proposal, phi=self.phi, scale_state=self.scale_state, scale_obs=self.scale_obs,
+                                    scale=self.scale)
 
         def sample(self, ancestor, observation):
-            return self.distribution.sample(args_loc=[ancestor, observation])
+            loc_args = np.array(list(zip(ancestor, [observation]*len(ancestor))), dtype=np.float64)
+            loc = wrapper_arr_arr(self.func_loc, loc_args)
+            scale = wrapper_arr(func_scale_proposal, np.array([self.scale]*len(ancestor), dtype=np.float64))
+            return self.distribution.sample(loc=loc, scale=scale)
 
         def density(self, particle, ancestor, observation):
-            return self.distribution.density(particle, args_loc=[ancestor, observation])
+            loc_args = np.array(list(zip(ancestor, [observation]*len(ancestor))), dtype=np.float64)
+            loc = wrapper_arr_arr(self.func_loc, loc_args)
+            scale = wrapper_arr(func_scale_proposal, np.array([self.scale]*len(ancestor), dtype=np.float64))
+            return self.distribution.density(particle, loc=loc, scale=scale)
 
     def observ_gen(self, length):
-        observ = []
-        state = []
-        x = self.initial.sample()
-        y = 0
+        observ = np.empty(length+1)
+        state = np.empty(length+1)
+        x = self.initial
         for i in range(length+1):
-            observ.append(self.conditional.sample(x))
-            state.append(x)
+            observ[i] = self.conditional.sample(x)[0]
+            state[i] = x[0]
             x = self.kernel.sample(x)
         return observ, state
-
-
-    # @staticmethod
-    # def normal_density(x, mean, sd):
-    #     return 1/(np.sqrt(2*np.pi)*sd)*np.exp(-1/(2*sd**2)*(x - mean)**2)
-    #
-    # def kernel_density(self, x, x_prev):
-    #     mean =self.phi*x_prev
-    #     out = KalmanMap.normal_density(x, mean, self.sigma_state)
-    #     return out
-
-    # def kernel(self, x):
-    #     mean = self.phi*x
-    #     return np.random.normal(mean, self.sigma_state)
-
-
-    # def conditional(self, x):
-    #     mean = x
-    #     out = np.random.normal(mean, self.sigma_obs)
-    #     return out
-    #
-    # def conditional_density(self, x, y):
-    #     mean = x
-    #     out = KalmanMap.normal_density(y, mean, self.sigma_obs)
-    #     return out
-
-    # def prior_proposal(self, x, y):
-    #     return self.kernel(x)
-    #
-    # def prior_proposal_density(self, x, x_prev, y):
-    #     return self.kernel_density(x, x_prev)
-
-    # def proposal(self, x, y):
-    #     coeff = (self.sigma_obs*self.sigma_state)**2/(self.sigma_obs**2+self.sigma_state**2)
-    #     mean = coeff*(self.phi*x/self.sigma_state**2 + y/self.sigma_obs**2)
-    #     sd = np.sqrt(coeff)
-    #     return np.random.normal(mean, sd)
-    #
-    # def proposal_density(self, x, x_prev, y):
-    #     coeff = (self.sigma_obs*self.sigma_state)**2/(self.sigma_obs**2+self.sigma_state**2)
-    #     mean = coeff*(self.phi*x_prev/self.sigma_state**2 + y/self.sigma_obs**2)
-    #     sd = np.sqrt(coeff)
-    #     return KalmanMap.normal_density(x, mean, sd)
---
+++ bootstrap/utils_kalman.pyx
@@ -0,0 +1,22 @@
+#cython: wraparound=False
+#cython: boundscheck=False
+#cython: cdivision=True
+#cython: nonecheck=False
+#cython: profile=False
+
+from libc.math cimport sqrt
+
+cpdef double func_loc_state(double arg, double phi):
+    return phi*arg
+
+cpdef double func_scale_state(double sigma):
+    return sigma
+
+cpdef double func_scale_obs(double sigma):
+    return sigma
+
+cpdef double func_loc_proposal(double[::1] args, double phi, double scale_state, double scale_obs, double scale):
+    return scale*(phi*args[0]/scale_state*scale_state + args[1]/scale_obs*scale_obs)
+
+cpdef double func_scale_proposal(double scale):
+    return sqrt(scale)
