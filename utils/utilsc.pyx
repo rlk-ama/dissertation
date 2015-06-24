25bcff3e59f860415a40192b994c329d567b1ee6
Raphael Lopez Kaufman <raphael.lopezkaufman@gmail.com>
2015-06-24 19:24:19 +0100

    removed decorators

--- utils/utilsc.pyx
+++ utils/utilsc.pyx
@@ -1,56 +1,12 @@
-from libc.math cimport exp, pow, log
-from collections import Iterable
+#cython: wraparound=False
+#cython: boundscheck=False
+#cython: cdivision=True
+#cython: nonecheck=False
+#cython: profile=False
+
 import numpy as np
 cimport numpy as np
-cimport cython
 
-@cython.cdivision(True)
-cpdef tuple param_gamma(double r, double sigma, double n_prev):
-    cdef double coeff, alpha, beta, var
-    var = sigma*sigma
-    coeff = r*n_prev*exp(-n_prev)
-    alpha = 1/var
-    beta = 1/alpha*exp(log(coeff) + var/2)
-    return alpha, beta
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.cdivision(True)
-cpdef double[:, ::1] param_gamma_arr(double r, double sigma, double[::1] n_prevs):
-    cdef int dim = n_prevs.shape[0]
-    cdef double[::1] alphas = np.empty(dim)
-    cdef double[::1] betas = np.empty(dim)
-    cdef double[:, ::1] output = np.empty((2, dim))
-    cdef double coeff, var
-    cdef int i
-    var = sigma*sigma
-    for i in range(dim):
-        coeff = r*n_prevs[i]*exp(-n_prevs[i])
-        alphas[i] = 1/var
-        betas[i] = 1/alphas[i]*exp(log(coeff) + var/2)
-    output[0] = alphas
-    output[1] = betas
-    return output
-
-
-cpdef double func_mean(double args, double r):
-    return log(r) + log(args) - args
-
-cpdef double func_sigma(double sigma):
-    return sigma
-
-cpdef double func_lam(double args, double phi):
-    return phi*args
-
-cpdef double func_shape(double[::1] args):
-    return args[0] + args[1]
-
-@cython.cdivision(True)
-cpdef double func_scale(double beta, double phi):
-    return beta/(beta*phi+1)
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
 cpdef double[::1] wrapper_arr(func, double[::1] args):
     cdef int dim = args.shape[0]
     cdef double[::1] output = np.empty(dim)
@@ -59,8 +15,6 @@ cpdef double[::1] wrapper_arr(func, double[::1] args):
         output[i] = func(args[i])
     return output
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
 cpdef double[::1] wrapper_arr_arr(func, double[:, ::1] args):
     cdef int dim = args.shape[0]
     cdef double[::1] output = np.empty(dim)
