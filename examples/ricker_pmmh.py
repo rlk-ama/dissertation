da8b0cc408437db8e5df9ca15fc628e00ab1e5c9
Raphael Lopez Kaufman <raphael.lopezkaufman@gmail.com>
2015-07-31 18:10:35 +0100

    support for generalized ricker

--- examples/ricker_pmmh.py
+++ examples/ricker_pmmh.py
@@ -1,5 +1,5 @@
 from bootstrap.filter import BootstrapFilter
-from bootstrap.ricker_gamma import RickerMap
+from bootstrap.ricker_gamma import RickerMap, RickerGeneralizedMap
 from proposals.proposals import RandomWalkProposal, MultivariateUniformProposal
 from distributions.distributions2 import Gamma, Normal
 from pmcmc.pmmh import PMMH
@@ -7,11 +7,18 @@ from pmcmc.pmmh import PMMH
 import argparse
 import numpy as np
 
-def simulation(r=44.7, phi=10, sigma=0.3, NOS=50, NBS=500, iter=17500, runs=10, burnin=2500, adaptation=2500,
-               r_init=40, phi_init=10, sigma_init=0.2, target=0.18, target_low=0.10, filter_proposal='optimal'):
+def simulation(r=44.7, phi=10, theta=1, sigma=0.3, NOS=50, NBS=500, iter=17500, runs=10, burnin=2500, adaptation=2500,
+               r_init=40, phi_init=10, theta_init=0.9, sigma_init=0.2, target=0.18, target_low=0.10, filter_proposal='optimal', generalized=False):
 
-    sigma_proposals = [5, 0.5, 0.1]
-    map_ = RickerMap
+    if generalized:
+        sigma_proposals = [5, 0.5, 0.05, 0.1]
+        initial_values = [r_init, phi_init, theta_init, sigma_init]
+        map_ = RickerGeneralizedMap
+    else:
+        sigma_proposals = [5, 0.5, 0.1]
+        initial_values = [r_init, phi_init, sigma_init]
+        map_ = RickerMap
+
     filter = BootstrapFilter
 
     initial_filter = {
@@ -21,23 +28,40 @@ def simulation(r=44.7, phi=10, sigma=0.3, NOS=50, NBS=500, iter=17500, runs=10, 
     }
 
     proposals = [RandomWalkProposal(sigma=sigma_proposal) for sigma_proposal in sigma_proposals]
-    prior = MultivariateUniformProposal(ndims=3, func_lows=[lambda args: np.exp(3.2), lambda args: 5, lambda args: 0.15],
-                                        func_highs=[lambda args: np.exp(4.17), lambda args: 15, lambda args: 0.45])
+
+    if generalized:
+        prior = MultivariateUniformProposal(ndims=4, func_lows=[lambda args: np.exp(3.2), lambda args: 5, lambda args: 0.8, lambda args: 0.15],
+                                            func_highs=[lambda args: np.exp(4.17), lambda args: 15, lambda args: 1.2, lambda args: 0.45])
+        support = lambda x: x[0] > np.exp(3.2)  and x[0] < np.exp(4.17) and x[1] > 5 and x[1] < 15 and  x[2] > 0.8 and x[2] < 1.2 and x[3] > 0.15 and x[3] < 0.45
+    else:
+        prior = MultivariateUniformProposal(ndims=3, func_lows=[lambda args: np.exp(3.2), lambda args: 5, lambda args: 0.15],
+                                            func_highs=[lambda args: np.exp(4.17), lambda args: 15, lambda args: 0.45])
+        support = lambda x: x[0] > np.exp(3.2)  and x[0] < np.exp(4.17) and x[1] > 5 and x[1] < 15 and x[2] > 0.15 and x[2] < 0.45
 
     for i in range(runs):
         initial_ricker = Gamma().sample(np.array([3], dtype=np.float64), np.array([1], dtype=np.float64))
-        Map = RickerMap(r, phi, sigma, length=NOS, initial=initial_ricker, approx='simple')
 
-        mcmc = PMMH(filter, map_, iter, proposals, prior, [r_init, phi_init, sigma_init], initial_ricker, 0, NOS, NBS, observations=Map.observations,
-                    support=lambda x: x[0] > np.exp(3.2)  and x[0] < np.exp(4.1) and x[1] > 5 and x[1] < 15 and x[2] > 0.15 and x[2] < 0.45,
-                    adaptation=adaptation, burnin=burnin, target=target, target_low=target_low, initial_filter=initial_filter,
+        if generalized:
+            Map = RickerGeneralizedMap(r, phi, theta, sigma, length=NOS, initial=initial_ricker, approx='simple')
+        else:
+            Map = RickerMap(r, phi, sigma, length=NOS, initial=initial_ricker, approx='simple')
+
+        mcmc = PMMH(filter, map_, iter, proposals, prior, initial_values, initial_ricker, 0, NOS, NBS, observations=Map.observations,
+                    support=support, adaptation=adaptation, burnin=burnin, target=target, target_low=target_low, initial_filter=initial_filter,
                     filter_proposal=filter_proposal)
 
         samples, acceptance = mcmc.sample()
-        thetas, phis, sigmas = zip(*samples)
+
+        if generalized:
+            rs, phis, thetas, sigmas = zip(*samples)
+        else:
+            rs, phis, sigmas = zip(*samples)
         acceptance_rate = np.sum(acceptance[burnin+adaptation:])/len(acceptance[burnin+adaptation:])
 
-        yield thetas, phis, sigmas, acceptance_rate
+        if generalized:
+            yield rs, phis, thetas, sigmas, acceptance_rate
+        else:
+            yield rs, phis, sigmas, acceptance_rate
 
 
 if __name__ == "__main__":
@@ -59,19 +83,20 @@ if __name__ == "__main__":
     parser.add_argument("--target_low", type=float, help="Minimum acceptance rate")
     parser.add_argument("--filter_proposal", type=str, help="Proposal for the particle fitler, either prior or optimal")
     parser.add_argument("--destination", type=str, help="Path of the destination of the simulations", required=True)
+    parser.add_argument("--generalized", type=bool, help="Are you using the Generalized Ricker map ?")
 
     args = parser.parse_args()
     arguments = {k:v for k,v in args.__dict__.items() if v is not None and k != 'destination'}
     path = args.destination
 
-    run = 0
+    run = 12
 
-    for thetas, phis, sigmas, acceptance_rate in simulation(**arguments):
+    for *variables, acceptance_rate in simulation(**arguments):
         with open(''.join([path, 'samples_{}.txt'.format(run)]) if path[-1] == '/' else '/'.join([path, 'samples_{}.txt'.format(run)]), 'w') as f:
             f.write(str(acceptance_rate))
             f.write("\n")
-            for j in range(len(thetas)):
-                f.write(" ".join(map(str,[thetas[j], phis[j], sigmas[j]])))
+            for j in range(len(variables[0])):
+                f.write(" ".join(map(str, [variable[j] for variable in variables])))
                 f.write("\n")
         print(run)
         run += 1
