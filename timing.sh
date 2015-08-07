9c72b88c75774bce14b121f707e5153318c29cdd
Raphael Lopez Kaufman <raphael.lopezkaufman@gmail.com>
2015-08-07 18:03:19 +0100

    fixed total timing calculations

--- timing.sh
+++ timing.sh
@@ -4,20 +4,21 @@ sum_prior=0
 dest="/home/raphael/profiling.txt"
 for (( i=0; i < number; i++ ))
 do
-    echo "toto"
+    echo "$i"
     kernprof -l "examples/ricker.py" --particles 5000
     file="/home/raphael/dissertation/ricker.py.lprof"
     python3 -m line_profiler $file > $dest
     t1=`awk '/proposal.sample/ {printf $4}' $dest`
     t2=`awk '/proposal.density/ {printf $4}' $dest`
-    sum_optimal=`echo "$t1+$t2" | bc`
+    sum_optimal=`echo "$sum_optimal+$t1+$t2" | bc`
     kernprof -l "examples/ricker.py" --filter_proposal prior --particles 5000
     python3 -m line_profiler $file > $dest
     t1=`awk '/prior.sample/ {printf $4}' $dest`
     t2=`awk '/prior.density/ {printf $4}' $dest`
-    sum_prior=`echo "$t1+$t2" | bc`
+    sum_prior=`echo "$sum_prior+$t1+$t2" | bc`
 done
 av_optimal=`echo "$sum_optimal/$number" | bc`
 av_prior=`echo "$sum_prior/$number" | bc`
 echo $av_optimal
 echo $av_prior
+echo "$av_optimal/$av_prior" | bc -l
