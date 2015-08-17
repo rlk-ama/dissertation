1aed24041b3fb1e65cd3d3605b828e2ef5518953
Raphael Lopez Kaufman <raphael.lopezkaufman@gmail.com>
2015-08-17 19:03:41 +0100

    transect lik plots for blowfly

---
+++ r-files/mleBlowfly.R
@@ -0,0 +1,13 @@
+params = c("p", "n0", "sigmap", "delta", "sigmad")
+
+for (param in params) {
+  data = read.table(sprintf("/home/raphael/mle_%s_blowfly.txt", param), header=F)
+  end = nrow(data)-1
+  dataFrame = data.frame(x=data[1:end, 1], y=data[1:end, 2])
+  ggplot(data=dataFrame, aes(x=x, y=y)) + geom_line() + 
+    geom_vline(xintercept=data[end+1, 1], colour="red", linetype="dashed") +
+    geom_vline(xintercept=data[end+1, 2], linetype="dashed") +
+    ylab("Loglikelihood") + xlab("Parameter")
+  ggsave(sprintf("/home/raphael/dissertation/figures/mleBlowfly%s.pdf", param), width=8, height=6)
+}
+
