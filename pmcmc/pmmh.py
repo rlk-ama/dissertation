e36603231438b1d3ecae0956f5ac05d5c9987a50
Raphael Lopez Kaufman <raphael.lopezkaufman@gmail.com>
2015-09-04 17:42:03 +0100

    new adaptation

--- pmcmc/pmmh.py
+++ pmcmc/pmmh.py
@@ -1,12 +1,13 @@
 import numpy as np
 from time import time
 from proposals.proposals import RandomWalkProposal
+from copy import deepcopy
 
 class PMMH(object):
 
     def __init__(self, filter, map, iterations, proposals, prior, init, initial, end, Ns, adaptation=1000,
-                 burnin=1500, target=0.15, target_low=0.10, observations=None, support=None, initial_filter=None,
-                 filter_proposal='optimal', *args, **kwargs):
+                 burnin=1500, target=0.2, target_low=0.10, observations=None, support=None, initial_filter=None,
+                 filter_proposal='optimal', split=50, *args, **kwargs):
         self.filter = filter
         self.map = map
         self.iterations = iterations
@@ -19,7 +20,7 @@ class PMMH(object):
         self.initial = initial
         self.adaptation = adaptation
         self.burnin = burnin
-        self.split = 100
+        self.split = split
         self.target = target
         self.target_low = target_low
         self.initial_filter = initial_filter
@@ -40,7 +41,7 @@ class PMMH(object):
         except:
             return -np.inf
         filter = self.filter(self.end, self.Ns, Map, proposal={self.filter_proposal: True}, initial=self.initial_filter,
-                             likeli=True)
+                             likeli=True, **self.kwargs)
         _, likeli= next(filter.filter())
         return likeli[-1]
 
@@ -50,19 +51,26 @@ class PMMH(object):
         return np.log(uniform_draw) < ratio
 
     def sub_sample(self, theta, likeli, index=None):
-        if index:
-            theta_star = theta
+        if index is not None:
+            theta_star = _copy(theta)
             theta_star[index] = RandomWalkProposal(sigma=self.proposals.lambdas[index]*self.proposals.cov[index, index]).sample(theta[index]).base
         else:
             theta_star = self.proposals.sample(theta)
         if self.support(theta_star):
             likeli_star = self.routine(theta_star)
-            numerator =  likeli_star + np.log(self.proposals.density(theta, theta_star)) + np.log(self.prior.density(np.array(theta_star)))
-            denominator = likeli + np.log(self.proposals.density(theta_star, theta)) + np.log(self.prior.density(np.array(theta)))
+            try:
+                numerator =  likeli_star + np.log(self.proposals.density(theta, theta_star)) + np.log(self.prior.density(np.array(theta_star)))
+                denominator = likeli + np.log(self.proposals.density(theta_star, theta)) + np.log(self.prior.density(np.array(theta)))
+            except:
+                numerator = -np.inf
+                denominator = 0
             if self.accept_reject(numerator-denominator):
                 theta = theta_star
                 likeli = likeli_star
-            accept = min(1, np.exp(numerator-denominator))
+            if numerator == -np.inf:
+                accept = 0
+            else:
+                accept = min(1, np.exp(numerator-denominator))
         else:
             accept = 0
         return theta, likeli, accept
@@ -80,9 +88,13 @@ class PMMH(object):
 
         start = self.burnin
         k = 2
+        counter = 0
+        iter = 0
+        end = 0
         while start < self.burnin + self.adaptation:
             end = start + self.split
-            index = list(np.random.multinomial(1, [1/len(theta)]*len(theta))).index(1)
+            #index = list(np.random.multinomial(1, [1/len(theta)]*len(theta))).index(1)
+            index = iter % len(theta)
             mean = []
             for iteration in range(start, end):
                 theta, likeli, accept = self.sub_sample(thetas[iteration], likeli, index)
@@ -90,24 +102,41 @@ class PMMH(object):
                 accepts.append(accept)
                 mean.append(theta)
                 print(iteration)
+            iter += 1
             acceptance_rate = np.sum(accepts[start:end])/self.split
-            print(acceptance_rate)
+            acceptance_rate_sofar = np.sum(accepts[:end])/end
+            print(acceptance_rate_sofar, acceptance_rate, index)
+            if abs(acceptance_rate_sofar-self.target) < 0.02:
+                counter += 1
+            else:
+                counter = 0
+            if counter >= 10*len(theta):
+                break
             self.rescale(acceptance_rate, k, index, mean)
             start = start + self.split
             k += 1
+        print(self.proposals.cov)
 
         for iteration in range(start, self.iterations):
             theta, likeli, accept = self.sub_sample(thetas[iteration], likeli)
             thetas.append(theta)
             accepts.append(accept)
             print(iteration)
+            if iteration % 100 == 0: print(np.sum(accepts[end:iteration])/(iteration-end))
 
         return thetas, accepts
 
     def rescale(self, acceptance_rate, k, index, mean):
         coeff = (acceptance_rate - self.target)/k
         self.proposals.lambdas[index] = self.proposals.lambdas[index]*np.exp(coeff)
-        self.proposals.mean = self.proposals.mean + 1/k*(np.mean(mean, axis=0)-self.proposals.mean)
         x = np.mean(mean, axis=0)-self.proposals.mean
         xT = x[:, np.newaxis]
-        self.proposals.cov = self.proposals.cov + 1/k*(x*xT)
+        self.proposals.mean = self.proposals.mean + 1/k*(np.mean(mean, axis=0)-self.proposals.mean)
+        self.proposals.cov = self.proposals.cov + 1/k*(x*xT-self.proposals.cov)
+
+
+def _copy(carray):
+    out = np.empty(len(carray))
+    for i in range(len(carray)):
+        out[i] = carray[i]
+    return out
